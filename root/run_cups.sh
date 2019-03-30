e6669edbb084f533d0e53884e1571e36f7e41bd7
John Mehan <john.mehan@irdeto.com>
2019-03-30 18:19:37 -0400

    Initial commit

---
+++ root/run_cups.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+set -e
+set -x
+
+if [ $(grep -ci $CUPSADMIN /etc/shadow) -eq 0 ]; then
+    adduser --ingroup lpadmin --no-create-home --disabled-password --gecos "" $CUPSADMIN
+    #adduser -S -G lpadmin --no-create-home $CUPSADMIN 
+fi
+echo $CUPSADMIN:$CUPSPASSWORD | chpasswd
+
+mkdir -p /config/ppd
+mkdir -p /services
+rm -rf /etc/avahi/services/*
+rm -rf /etc/cups/ppd
+ln -s /config/ppd /etc/cups
+if [ `ls -l /services/*.service 2>/dev/null | wc -l` -gt 0 ]; then
+	cp -f /services/*.service /etc/avahi/services/
+fi
+if [ `ls -l /config/printers.conf 2>/dev/null | wc -l` -eq 0 ]; then
+    touch /config/printers.conf
+fi
+cp /config/printers.conf /etc/cups/printers.conf
+
+/etc/init.d/dbus start
+/usr/sbin/avahi-daemon --daemonize
+/root/printer-update.sh &
+exec /usr/sbin/cupsd -f
